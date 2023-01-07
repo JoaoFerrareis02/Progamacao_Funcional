eb1a185d97d4d36cf91f86808f6e0d7cc06d95e8
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-01-07 12:56:22 -0300

    Commit de progamas (Pipeline)

--- src/application/Program5.java
+++ src/application/Program5.java
@@ -21,7 +21,7 @@ public class Program5 {
 		System.out.println(Arrays.toString(st3.limit(10).toArray()));
 		
 		Stream<Long> st4 = Stream.iterate(new long[] { 0L, 1L }, p -> new long[] { p[1], p[0] + p[1] }).map(p -> p[0]);
-		System.out.println(Arrays.toString(st4.limit(10).toArray()));
+		System.out.println(Arrays.toString(st4.limit(45).toArray()));
 
 	}
 
---
+++ src/application/Program6.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Program6 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		
+		Stream<Integer> st1 = list.stream().map(x -> x*10);
+		System.out.println(Arrays.toString(st1.toArray()));
+		
+		int sum = list.stream().reduce(0, (x,y) -> x+y);
+		System.out.println("Sum = "+sum);
+		
+		List<Integer> newList = list.stream()
+				.filter(x -> x%2 == 0)
+				.map(x -> x*10)
+				.collect(Collectors.toList());
+		
+		System.out.println(Arrays.toString(newList.toArray()));
+
+	}
+
+}
