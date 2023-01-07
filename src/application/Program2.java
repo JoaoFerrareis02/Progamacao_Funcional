7d6d10e8e120920c4174234048452be737e79efb
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-01-07 10:41:05 -0300

    Commit de progamas (O que é progamação funcional?)

---
+++ src/application/Program2.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		list.sort((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
+		list.forEach(System.out::println);
+	}
+
+}
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -30,7 +30,8 @@ public class Product implements Comparable<Product>{
 	public String toString() {
 		return "Product [name=" + name + ", price=" + price + "]";
 	}
-
+	
+	
 	@Override
 	public int compareTo(Product o) {
 		return name.toUpperCase().compareTo(o.getName().toUpperCase());
